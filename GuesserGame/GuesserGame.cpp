c1d66340be6db65bc6ff7ad6775a45f66fd0e8f8
RealCrayfish <realcrayfish896@gmail.com>
2022-04-17 23:38:17 +0100

    added a broken exit menu

--- GuesserGame/GuesserGame.cpp
+++ GuesserGame/GuesserGame.cpp
@@ -4,6 +4,7 @@
 #include "GuesserGame.h"
 
 #include "ftxui/component/captured_mouse.hpp"
+#include "ftxui/component/event.hpp"
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/component_base.hpp"
 #include "ftxui/component/screen_interactive.hpp"
@@ -12,31 +13,43 @@
 using namespace std;
 using namespace ftxui;
 
-int exitGame() {
-    
-
-    return 0
-}
-
-int main()
-{
-    int value = 50;
-    auto action = [&] { value++; };
-
-    auto logo = vbox({
+// Game Logo
+auto logo = vbox({
         text("░██████╗░██╗░░░██╗███████╗░██████╗░██████╗███████╗██████╗░░██████╗░░█████╗░███╗░░░███╗███████╗"),
         text("██╔════╝░██║░░░██║██╔════╝██╔════╝██╔════╝██╔════╝██╔══██╗██╔════╝░██╔══██╗████╗░████║██╔════╝"),
         text("██║░░██╗░██║░░░██║█████╗░░╚█████╗░╚█████╗░█████╗░░██████╔╝██║░░██╗░███████║██╔████╔██║█████╗░░"),
         text("██║░░╚██╗██║░░░██║██╔══╝░░░╚═══██╗░╚═══██╗██╔══╝░░██╔══██╗██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░"),
         text("╚██████╔╝╚██████╔╝███████╗██████╔╝██████╔╝███████╗██║░░██║╚██████╔╝██║░░██║██║░╚═╝░██║███████╗"),
         text("░╚═════╝░░╚═════╝░╚══════╝╚═════╝░╚═════╝░╚══════╝╚═╝░░╚═╝░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝"),
+    });
+
+// Exit Game Code
+int exitGame() {
+    auto screen = ScreenInteractive::FitComponent();
+
+    auto component = Renderer([&] {
+        return vbox({
+                logo,
+            }) |
+            border;
         });
+
+    screen.Loop(component);
+
+    return 0;
+}
+
+// Run Game
+int main()
+{
+    int value = 50;
+    auto action = [&] { value++; };
 
     auto buttons = Container::Vertical({
         Button("New Game", action, ButtonOption::Animated()),
         Button("Scoreboard", action, ButtonOption::Animated()),
         Button("Credits", action, ButtonOption::Animated()),
-        Button("Exit Game", action, ButtonOption::Animated()),
+        Button("Exit Game", exitGame, ButtonOption::Animated()),
     });
 
     auto component = Renderer(buttons, [&] {
